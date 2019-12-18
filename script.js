1242eee78363c689c88616a25145ca8afa9924d1
fightingox1 <44482532+fightingox1@users.noreply.github.com>
2019-12-18 13:01:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -91,8 +91,7 @@ function sendMessage() {
   DOM.input.value = '';
   drone.publish()
     room: 'observable-room',
-    message: value,
-  });
+    message: 'value',
 }
 
 function createMemberElement(member) {
